2e4b47ecac2763df9388b4cbecb9b5b5329d2bef
Matthew Joughin <matthew@adaptableapps.net>
2023-08-04 16:30:22 +0200

    Working on this
    Signed-off-by: Matthew Joughin <matthew@adaptableapps.net>

---
+++ examples/dotnet/weatherit/scripts/_weatherit_codegen_cs.ps1
@@ -0,0 +1,38 @@
+param(
+    [string]$WI_ROOT_PATH,
+    [string]$WI_RELEASE_VERSION_NUMBER_PADDED_FILE_NAME_FRIENDLY
+)
+
+$ValidData = $true        # the tests will set this to false if we find bad data.
+
+if ([string]::IsNullOrWhiteSpace($WI_ROOT_PATH))
+{
+    Write-Host "Parameter 1 is empty - please specify the WI Root Path";
+    $ValidData = $false
+}
+
+if ([string]::IsNullOrWhiteSpace($WI_RELEASE_VERSION_NUMBER_PADDED_FILE_NAME_FRIENDLY))
+{
+    Write-Host "Parameter 2 is empty - please specify the WI Release Version Number Padded File Name Friendly";
+    $ValidData = $false
+}
+
+if ($ValidData)
+{
+    Write-Host ""
+    Write-Host "Parameter 1 (WI Root Path) : $WI_ROOT_PATH"
+
+    Write-Host ""
+    Write-Host "Parameter 2 (WI Release Version Number Padded File Name Friendly) : $WI_RELEASE_VERSION_NUMBER_PADDED_FILE_NAME_FRIENDLY"
+
+    $WI_CODEGEN_DATA_FILE = "$Env:WI_ROOT\src\aa.cs.code.gen\Data\wi.code.gen.data_WeatherIt_$WI_RELEASE_VERSION_NUMBER_PADDED_FILE_NAME_FRIENDLY.xlsx"
+
+    Write-Host ""
+
+    Write-Host "WI_CODEGEN_DATA_FILE = $WI_CODEGEN_DATA_FILE"
+
+    Write-Host ""
+
+    Write-Host sourcedynamo action=codegen codegendatafile="$WI_CODEGEN_DATA_FILE" productrootpath="$WI_ROOT_PATH"
+    sourcedynamo action=codegen codegendatafile="$WI_CODEGEN_DATA_FILE" productrootpath="$WI_ROOT_PATH"
+}
